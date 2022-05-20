21b0badb44d51206230944fdbbb8b7fa82d6b0bd
Thomas Barnwell <thomas@krakenlabs.io>
2022-05-19 21:39:58 -0400

    login work

--- screens/Welcome/index.js
+++ screens/Welcome/index.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { render, Text } from "ink";
+import React, { useState, useEffect } from "react";
+import { Text, Box, Newline } from "ink";
+import TextInput from "ink-text-input";
+
+let userName = "";
+
+const welcomeString = "Welcome to the Honeypot BBS";
 
 const Welcome = () => {
 	return (
 		<>
-			<Text color="green">test2</Text>
+			<Box
+				padding={1}
+				borderStyle="round"
+				borderColor="green"
+				width={welcomeString.length + 4}
+			>
+				<Text>{welcomeString}</Text>
+			</Box>
+			<Newline />
+			<TextInput
+				value={userName}
+				onChange={(e) => {
+					// console.log(e);
+				}}
+			/>
 		</>
 	);
 };
